583171a8c0c021d72004000fbf0bb0b00e3f9511
Adrian Julianto <juliantoadrian@gmail.com>
2019-08-12 11:24:05 +0700

    Browin Initial commit

---
+++ lib/view_tool_browin.rb
@@ -0,0 +1,7 @@
+require "view_tool_browin/version"
+require "view_tool_browin/renderer"
+
+module ViewToolBrowin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/view_tool_browin/version.rb
@@ -0,0 +1,3 @@
+module ViewToolBrowin
+  VERSION = "0.1.0"
+end
